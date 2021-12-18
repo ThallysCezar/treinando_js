455f014e50f7972708828b91ead59f79569ddfae
ThallysCezar <75546628+ThallysCezar@users.noreply.github.com>
2021-12-17 21:45:12 -0300

    Test: change js

---
+++ script.js
@@ -0,0 +1,12 @@
+        // tipo string
+        var texto = "Curso de JavaScript"
+        // tipo number
+        var numeroInteiro = -7
+        // tipo boolean
+        var booleano = true
+
+        alert(numeroInteiro)
+
+        document.write(texto)
+
+        console.log()
